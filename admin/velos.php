e5b30c27499e25dc7ca2cd5688a501eedabc8623
ThomasDlv0 <thomas.delville000@icloud.com>
2026-01-09 18:15:55 +0100

    page vélos dans le amdin

--- admin/velos.php
+++ admin/velos.php
@@ -34,7 +34,6 @@ $pageTitle = "Gestion des vélos";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?> - RESAVELO Admin</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/admin.css">
 </head>
